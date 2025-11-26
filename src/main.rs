b812f884a8d9e0ea3822394dca7b48b613d59ed0
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-11-26 16:11:01 +0800

    Add tests

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use wasm_random::random;
+
+fn main() {
+    let random_u32 = getrandom::u32().unwrap();
+    println!("random u32: {}", random_u32);
+
+    let random_not_precise = random::random();
+    println!("inprecise random f32: {}", random_not_precise);
+
+    let random_precise = random::random_with_precision();
+    println!("precise random f32: {}", random_precise);
+
+    let random_range = random::random_from_range(100., 1720.);
+    println!("random range a: {}", random_range);
+
+    let random_range_b = random::random_from_range(0., 0.7);
+    println!("random range b: {}", random_range_b);
+}
--- src/random.rs
+++ src/random.rs
@@ -6,19 +6,74 @@ pub fn random() -> f32 {
     result
 }
 
-/// Generate a random number between 0 and 1 with more precision.
+#[test]
+fn test_random() {
+    let random_number = random();
+    println!("Random number: {}", random_number);
+    assert!(random_number >= 0., "Random number is or is above 0.");
+    assert!(random_number <= 1., "Random number is or is below 1.");
+    assert!(random_number.is_finite(), "Random number is finite");
+}
+
+/// Generate a random number between 0 and 1 with more precision than `random()`.
 /// The result is a float with more decimal places.
-pub fn random_more_precision() -> f32 {
+pub fn random_with_precision() -> f32 {
     let random_u32 = getrandom::u32().unwrap();
-    let result = (random_u32 as f32 % 1000.) / 1000.;
+    let result = (random_u32 as f32 % 10000000.) / 10000000.;
 
     result
 }
 
+#[test]
+fn test_random_with_precision() {
+    let random_number = random_with_precision();
+    println!("Random number: {}", random_number);
+    assert!(random_number >= 0., "Random number is or is above 0.");
+    assert!(random_number <= 1., "Random number is or is below 1.");
+    assert!(
+        random_number * 1000. >= 0.,
+        "Random number is at target precision"
+    );
+    assert!(random_number.is_finite(), "Random number is finite");
+}
+
 /// Generate a random number between min and max.
 pub fn random_from_range(min: f32, max: f32) -> f32 {
-    let random_seed = random();
+    let random_seed = random_with_precision();
     let result = min + random_seed * (max - min);
 
     result
+}
+
+#[test]
+fn test_random_from_range_decimal() {
+    let random_number = random_from_range(0., 1.);
+    assert!(random_number >= 0., "Random number is or is above 0.");
+    assert!(random_number <= 1., "Random number is or is below 1.");
+    assert!(
+        (random_number * 10. >= 0.) && (random_number * 10. <= 10.),
+        "Random number is at target precision"
+    );
+    assert!(random_number.is_finite(), "Random number is finite");
+}
+
+#[test]
+fn test_random_from_range_decimal_precise() {
+    let min = 0.15;
+    let max = 0.768;
+    let random_number = random_from_range(min, max);
+    assert!(random_number >= min, "Random number is or is above min");
+    assert!(random_number <= max, "Random number is or is below max");
+    assert!(random_number.is_finite(), "Random number is finite");
+}
+
+#[test]
+fn test_random_from_range_integer() {
+    let min = 300.;
+    let max = 1500.;
+    let random_number = random_from_range(min, max);
+    println!("Random number: {}", random_number);
+    assert!(random_number >= min, "Random number is or is above min");
+    assert!(random_number <= max, "Random number is or is below max");
+    assert!(random_number.is_finite(), "Random number is finite");
 }
