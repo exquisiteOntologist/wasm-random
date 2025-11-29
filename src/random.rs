22d579a4409651d3bf7c8cc351f0b8c5bb4f319c
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-11-29 10:04:35 +0800

    Add test module to reduce build size

--- src/random.rs
+++ src/random.rs
@@ -8,15 +8,6 @@ pub fn random() -> f32 {
     result
 }
 
-#[test]
-fn test_random() {
-    let random_number = random();
-    assert!(random_number >= 0.);
-    assert!(random_number <= 1.);
-    assert!((random_number * PRECISION_F32) >= 1.);
-    assert!(random_number.is_finite());
-}
-
 const PRECISION_F64: f64 = 10000000000000000000.;
 
 /// Generate a random number `f64` between `0.` and `1.`
@@ -25,15 +16,6 @@ pub fn random_f64() -> f64 {
     let result = (random_u64 as f64 % PRECISION_F64) / PRECISION_F64;
 
     result
-}
-
-#[test]
-fn test_random_f64() {
-    let random_number = random_f64();
-    assert!(random_number >= 0.);
-    assert!(random_number <= 1.);
-    assert!((random_number * PRECISION_F64) >= 1.);
-    assert!(random_number.is_finite());
 }
 
 /// Generate a random number between min and max.
@@ -46,45 +28,6 @@ pub fn random_from_range(min: f32, max: f32) -> f32 {
     result
 }
 
-#[test]
-fn test_random_from_range_a() {
-    let random_number = random_from_range(0., 1.);
-    assert!(random_number >= 0.);
-    assert!(random_number <= 1.);
-    assert!((random_number * 10. >= 0.) && (random_number * 10. <= 10.),);
-    assert!(random_number.is_finite());
-}
-
-#[test]
-fn test_random_from_range_b() {
-    let min = 0.15;
-    let max = 0.768;
-    let random_number = random_from_range(min, max);
-    assert!(random_number >= min);
-    assert!(random_number <= max);
-    assert!(random_number.is_finite());
-}
-
-#[test]
-fn test_random_from_range_c() {
-    let min = 0.35;
-    let max = 0.42;
-    let random_number = random_from_range(min, max);
-    assert!(random_number >= min);
-    assert!(random_number <= max);
-    assert!(random_number.is_finite());
-}
-
-#[test]
-fn test_random_from_range_d() {
-    let min = 300.;
-    let max = 1500.;
-    let random_number = random_from_range(min, max);
-    assert!(random_number >= min);
-    assert!(random_number <= max);
-    assert!(random_number.is_finite());
-}
-
 /// Generate a random number between min and max.
 pub fn random_from_range_f64(min: f64, max: f64) -> f64 {
     debug_assert!(min <= max);
@@ -95,11 +38,75 @@ pub fn random_from_range_f64(min: f64, max: f64) -> f64 {
     result
 }
 
-#[test]
-fn test_random_from_range_f64_a() {
-    let random_number = random_from_range_f64(0., 1.);
-    assert!(random_number >= 0.);
-    assert!(random_number <= 1.);
-    assert!((random_number * 10. >= 0.) && (random_number * 10. <= 10.),);
-    assert!(random_number.is_finite());
+#[cfg(test)]
+mod tests {
+    use crate::random::{
+        PRECISION_F32, PRECISION_F64, random, random_f64, random_from_range, random_from_range_f64,
+    };
+
+    #[test]
+    fn test_random() {
+        let random_number = random();
+        assert!(random_number >= 0.);
+        assert!(random_number <= 1.);
+        assert!((random_number * PRECISION_F32) >= 1.);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_f64() {
+        let random_number = random_f64();
+        assert!(random_number >= 0.);
+        assert!(random_number <= 1.);
+        assert!((random_number * PRECISION_F64) >= 1.);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_a() {
+        let random_number = random_from_range(0., 1.);
+        assert!(random_number >= 0.);
+        assert!(random_number <= 1.);
+        assert!((random_number * 10. >= 0.) && (random_number * 10. <= 10.),);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_b() {
+        let min = 0.15;
+        let max = 0.768;
+        let random_number = random_from_range(min, max);
+        assert!(random_number >= min);
+        assert!(random_number <= max);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_c() {
+        let min = 0.35;
+        let max = 0.42;
+        let random_number = random_from_range(min, max);
+        assert!(random_number >= min);
+        assert!(random_number <= max);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_d() {
+        let min = 300.;
+        let max = 1500.;
+        let random_number = random_from_range(min, max);
+        assert!(random_number >= min);
+        assert!(random_number <= max);
+        assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_f64_a() {
+        let random_number = random_from_range_f64(0., 1.);
+        assert!(random_number >= 0.);
+        assert!(random_number <= 1.);
+        assert!((random_number * 10. >= 0.) && (random_number * 10. <= 10.),);
+        assert!(random_number.is_finite());
+    }
 }
