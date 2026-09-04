1fdca044586dc19331d277531aa9dd478a1a65e2
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2026-09-04 11:13:45 +0800

    Update dependency getrandom, change tests for expression-based random generations

--- src/random.rs
+++ src/random.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Mul, Range, Sub};
+
 const PRECISION_F32: f32 = 100000000.;
 
 /// Generate a random number `f32` between `0.` and `1.`
@@ -38,10 +40,54 @@ pub fn random_from_range_f64(min: f64, max: f64) -> f64 {
     result
 }
 
+/// Generate a random number within the given range.
+pub fn random_from_range_exp(range: Range<i64>) -> i64 {
+    let start = range.start as f64;
+    let end = range.end as f64;
+
+    debug_assert!(start <= end);
+
+    let random_seed = random_f64();
+    let result = start + random_seed * (end - start);
+
+    result as i64
+}
+
+/// Generate a random number within the given range.
+pub fn random_from_range_exp_f64(range: Range<f64>) -> f64 {
+    debug_assert!(range.start <= range.end);
+
+    let random_seed = random_f64();
+    let result = range.start + random_seed * (range.end - range.start);
+
+    result
+}
+
+// pub fn random_from_range_exp<T>(range: Range<T>) -> T
+// where
+//     // can convert T Into f32
+//     // can conver From f32 to T
+//     T: Into<f32> + From<f32> + Copy,
+// {
+//     let start: f32 = range.start.into();
+//     let end: f32 = range.end.into();
+
+//     debug_assert!(start <= end);
+
+//     let random_seed = random();
+//     let result = start + random_seed * (end - start);
+
+//     result.into()
+// }
+
 #[cfg(test)]
 mod tests {
-    use crate::random::{
-        PRECISION_F32, PRECISION_F64, random, random_f64, random_from_range, random_from_range_f64,
+    use crate::{
+        random::{
+            PRECISION_F32, PRECISION_F64, random, random_f64, random_from_range,
+            random_from_range_f64,
+        },
+        random_from_range_exp, random_from_range_exp_f64,
     };
 
     #[test]
@@ -108,5 +154,21 @@ mod tests {
         assert!(random_number <= 1.);
         assert!((random_number * 10. >= 0.) && (random_number * 10. <= 10.),);
         assert!(random_number.is_finite());
+    }
+
+    #[test]
+    fn test_random_from_range_exp_a() {
+        let random_number = random_from_range_exp(0..10);
+        println!("exp_a {}", random_number);
+        assert!(random_number >= 0);
+        assert!(random_number <= 10);
+    }
+
+    #[test]
+    fn test_random_from_range_exp_f64_a() {
+        let random_number = random_from_range_exp_f64((0.)..10.);
+        println!("exp_f64_a {}", random_number);
+        assert!(random_number >= 0.);
+        assert!(random_number <= 10.);
     }
 }
